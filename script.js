5020ece59000cd55b769901a67b6b0314db91771
okitsF <hrd609@gmail.com>
2023-12-26 12:47:26 +0530

    1st

---
+++ script.js
@@ -0,0 +1,28 @@
+
+const btnw=150;
+const btnh=50;
+
+const maxw = window.innerWidth - btnw;
+const maxh = window.innerHeight - btnh;
+
+window.addEventListener('DOMContentLoaded',()=>{
+    const button = document.getElementById('btn');
+    const btnYes = document.getElementById('btn-yes');
+    const line = document.getElementById('line');
+
+
+    button.addEventListener('click', ()=>{
+        alert('clicked !!')
+    })
+    button.addEventListener('mouseover',()=>{
+        button.style.left=Math.floor(Math.random() * (maxw + 1)) + 'px';
+        button.style.top=Math.floor(Math.random() * (maxh + 1)) + 'px';
+
+    });
+    
+    btnYes.addEventListener("click",()=>{
+      line.innerHTML="ok lets go!! ";
+    });
+
+
+});
